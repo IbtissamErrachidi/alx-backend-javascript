acee83d164dde02e53679cf00680b23be8ae434c
IbtissamErrachidi <ibtissamerrachidi810@gmail.com>
2024-06-30 18:12:46 +0100

    user

--- 0x01-ES6_promise/4-user-promise.js
+++ 0x01-ES6_promise/4-user-promise.js
@@ -1,9 +1,13 @@
 export default function signUpUser(firstName, lastName) {
-  return new Promise((resolve) => {
-    resolve({
-      firstName: firstName,
-      lastName: lastName
-    });
+  return new Promise((resolve, reject) => {
+    if (firstName && lastName) {
+      resolve({
+        firstName: firstName,
+        lastName: lastName
+      });
+    } else {
+      reject(new Error("First name and last name are required"));
+    }
   });
 }
 
