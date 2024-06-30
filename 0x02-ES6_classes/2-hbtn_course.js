34a00aa6935f0a11e1143077cb4c57f1f9ff3369
IbtissamErrachidi <ibtissamerrachidi810@gmail.com>
2024-06-30 19:53:53 +0100

    hbtn

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -6,46 +6,40 @@ class HolbertonCourse {
     this.students = students;
   }
 
-  // name
+  // Getter et Setter pour name
   get name() {
     return this._name;
   }
 
   set name(name) {
-    if (typeof name === 'string') {
-      this._name = name;
-    } else {
+    if (typeof name !== 'string') {
       throw new TypeError('Name must be a string');
     }
+    this._name = name;
   }
 
-  // length
+  // Getter et Setter pour length
   get length() {
     return this._length;
   }
 
   set length(length) {
-    if (typeof length === 'number') {
-      this._length = length;
-    } else {
+    if (typeof length !== 'number') {
       throw new TypeError('Length must be a number');
     }
+    this._length = length;
   }
 
-  // students
+  // Getter et Setter pour students
   get students() {
     return this._students;
   }
 
   set students(students) {
-    if (
-      Array.isArray(students)
-      && students.every((s) => typeof s === 'string')
-    ) {
-      this._students = students;
-    } else {
+    if (!Array.isArray(students) || !students.every(student => typeof student === 'string')) {
       throw new TypeError('Students must be an array of strings');
     }
+    this._students = students;
   }
 }
 
