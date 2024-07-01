93369701fa7279745ce39556415939f5e042e4c6
IbtissamErrachidi <ibtissamerrachidi810@gmail.com>
2024-07-01 01:19:42 +0100

    manipulation

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,4 +1,7 @@
 export default function cleanSet(set, startString) {
+  if (startString === '' || typeof startString !== 'string') {
+    return '';
+  }
   let result = '';
   set.forEach((value) => {
     if (value.startsWith(startString)) {
